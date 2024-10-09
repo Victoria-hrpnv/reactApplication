d2ddbfdfef45984dd3093e2b22689ebbc2daf8e2
vikah <vikahrapina00@mail.ru>
2024-10-09 15:56:29 +0300

    /

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // base: 'https://victoria-hrpnv.github.io/reactApplication/',
   plugins: [react()],
-  base: '/reactApplication/'
+  base: '/reactApplication/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react'],
+        },
+      },
+    },
+  },
 })
